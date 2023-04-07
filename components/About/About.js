6c03b34d2b7eb511be4abbed483ae6a35614da76
JsArafath <arafath.91289@gmail.com>
2023-04-07 11:21:48 +0600

    About-Section-Implement

--- components/About/About.js
+++ components/About/About.js
@@ -21,11 +21,11 @@ const About = () => {
   <div> 
   
     {/* <---------------About Part-------------> */}
-  <div class="mx-auto flex justify-center items-center bg-white p-5 sm:flex">
-      <img class="h-36" src="https://i.ibb.co/QHjcc5v/Screenshot-2023-03-31-100804-removebg-preview.png" alt="" />
-      <div class="font-medium text-gray-800">
-          <div className="text-2xl">1.351.003</div>
-          <div class="text-sm text-gray-500 dark:text-gray-400">Pohon yang Tertanam</div>
+  <div className="mx-auto flex justify-center items-center bg-white p-5 sm:flex">
+      <img className="h-36" src="https://i.ibb.co/QHjcc5v/Screenshot-2023-03-31-100804-removebg-preview.png" alt="" />
+      <div className="font-medium text-gray-800">
+          <div className="text-2xl">1000+</div>
+          <div className="text-sm text-gray-500 dark:text-gray-400">Active Organization</div>
       </div>
       <br></br>
   
@@ -33,24 +33,24 @@ const About = () => {
   
   {/* <------------------------------> */}
   
-  <div class=" text-center bg-white m-5 sm:p-8 ">
+  <div className=" text-center bg-white m-5 sm:p-8 ">
   
-      <div class="justify-between p-5 space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-            <div class=" sm:w-auto bg-white  focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
-              <div class="text-left">
-              <img style={styleObject.size} class="" src="https://i.ibb.co/hcrYTBs/Untitled-3.png" alt="" />
+      <div className="justify-between p-5 space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+            <div className=" sm:w-auto bg-white  focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+              <div className="text-left">
+              <img style={styleObject.size} className="" src="https://i.ibb.co/hcrYTBs/Untitled-3.png" alt="" />
                   
               </div>
             </div>
   
-          <div class=" sm:w-auto bg-white  focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center p-5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
-              <div class="text-left">
-              <p style={styleObject.textcolor} className="text-gray-900 font-semibold pb-2">Tentang  Kami</p>
-                      <h1  class="text-4xl font-semibold text-gray-600 dark:text-white">BERTUMBUH - Save The<br></br> World </h1>
+          <div className=" sm:w-auto bg-white  focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center p-5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+              <div className="text-left">
+              <p style={styleObject.textcolor} className="text-gray-900 font-semibold pb-2">Managing Organization</p>
+                      <h1  className="text-4xl font-semibold text-gray-600 dark:text-white">We Commit To Manage <br></br> Your Organization </h1>
                       <br></br>
-                      <p class="text-base font-normal text-gray-500 dark:text-gray-400">BERTUMBUH - Adalah organisasi non-profit yang mempunyai   <br></br> Melindungi, menjaga, merawat, dan melestarikan pohon-  <br></br> bumi yang kita pijak sekarang. BERTUMBUH - Adalah  <br></br> non-profit yang mempunyai tujuan Melindungi, menjaga, <br></br> dan melestarikan pohon-pohon yang </p>
+                      <p className="text-base font-normal text-gray-500 dark:text-gray-400">BERTUMBUH - Adalah organisasi non-profit yang mempunyai   <br></br> Melindungi, menjaga, merawat, dan melestarikan pohon-  <br></br> bumi yang kita pijak sekarang. BERTUMBUH - Adalah  <br></br> non-profit yang mempunyai tujuan Melindungi, menjaga, <br></br> dan melestarikan pohon-pohon yang </p>
                       <br></br>
-                      <button style={styleObject.button} type="button" class="text-white bg-gray-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">Lihat Selengkapnya</button>
+                      <button style={styleObject.button} type="button" className="text-white bg-gray-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">   Start A Free Trail</button>
   
               </div>
               <br></br>
@@ -64,19 +64,19 @@ const About = () => {
   <div className="text-center bg-white m-5 sm:p-8 ">
   
       <div className="m-5 justify-between space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-          <div class="sm:w-auto bg-white  focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center p-5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+          <div className="sm:w-auto bg-white  focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center p-5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
               <div className="text-left">
-              <p style={styleObject.textcolor} className="text-gray-900 font-semibold ">Kami Butuh Kamu</p>
-                      <h1  class="text-4xl font-semibold text-gray-600 dark:text-white mr-5">Kenapa BERTUMBUH <br></br> Butuh Kamu ? </h1>
+              <p style={styleObject.textcolor} className="text-gray-900 font-semibold ">Managing Man-Power</p>
+                      <h1  className="text-4xl font-semibold text-gray-600 dark:text-white mr-5">We Will Manage  <br></br> Your Man-Power Carefully! </h1>
                       <br></br>
-                      <p class="text-base font-normal text-gray-500 dark:text-gray-400">BERTUMBUH - Adalah organisasi non-profit yang mempunyai   <br></br> Melindungi, menjaga, merawat, dan melestarikan pohon-  <br></br> bumi yang kita pijak sekarang. BERTUMBUH - Adalah  <br></br> non-profit yang mempunyai tujuan Melindungi, menjaga, <br></br> dan melestarikan pohon-pohon yang </p>
+                      <p className="text-base font-normal text-gray-500 dark:text-gray-400">BERTUMBUH - Adalah organisasi non-profit yang mempunyai   <br></br> Melindungi, menjaga, merawat, dan melestarikan pohon-  <br></br> bumi yang kita pijak sekarang. BERTUMBUH - Adalah  <br></br> non-profit yang mempunyai tujuan Melindungi, menjaga, <br></br> dan melestarikan pohon-pohon yang </p>
                       <br></br>
-                      <button style={styleObject.button} type="button" class="text-white bg-gray-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">Gabung Bertumbuh</button>
+                      <button style={styleObject.button} type="button" className="text-white bg-gray-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">Manage Your Power</button>
               </div>
           </div>
           <div className="sm:w-auto bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
               <div className="text-left">
-              <img style={styleObject.size} class="" src="https://i.ibb.co/DpydcPf/Untitledujnjikuu-copy.jpg" alt="" />
+              <img style={styleObject.size} className="" src="https://i.ibb.co/DpydcPf/Untitledujnjikuu-copy.jpg" alt="" />
                   
               </div>
           </div>
